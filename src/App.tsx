8e9acadf9e7a2d0e64fd3fd250b9f84c3129597c
Márton Zsombor Magyar <Marton.Magyar@student.lut.fi>
2026-09-21 18:02:25 +0300

    Added scroll to top function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import About from './pages/about'
 import ProjectsPage from './pages/projects'
+
+function ScrollToTop() {
+  const { pathname, search } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname, search])
+
+  return null
+}
 
 function App() {
   return (
     <>
       <div className="ambient-background" aria-hidden="true" />
       <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <ScrollToTop />
         <div style={{ position: 'relative', zIndex: 1 }}>
           <a className="skip-link" href="#main">
             Skip to main content
